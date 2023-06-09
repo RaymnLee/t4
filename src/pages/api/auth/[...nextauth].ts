39dd9e12849bf2ff059979e4ab0b7a0a2cbbcd92
RaymnLee <legollee44@gmail.com>
2023-06-10 04:33:06 +0800

    2nd

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,15 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
+import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth(authOptions);
+
+export default NextAuth({
+  ...authOptions,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // Other providers if needed
+  ],
+});
